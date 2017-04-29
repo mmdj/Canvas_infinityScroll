df803c3bdb2230cc6163f36e94c99db1204d4d36
mmdj <mdolzhansky@gmail.com>
2017-04-29 19:47:04 +0300

    1

---
+++ app/src/main/java/com/mdolzhansky/canvas_infinityscroll/DrawView.java
@@ -0,0 +1,249 @@
+package com.mdolzhansky.canvas_infinityscroll;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import com.mdolzhansky.canvas_infinityscroll.model.Cell;
+
+/**
+ *  Created by mmdj on 29.04.2017.
+ */
+
+class DrawView extends SurfaceView implements SurfaceHolder.Callback {
+    SurfaceHolder surfaceHolder;
+    private int screenWidth;
+    private int screenHeight;
+
+    private DrawThread drawThread;
+    private Paint paint;
+    private Rect rect;
+    private Cell cell;
+
+    private int CELL_WIDTH = 250;
+    private int CELL_HEIGHT = 250;
+    private int START_POSITION_X = 0;
+    private int START_POSITION_Y = 0;
+    int nCellsHorizontal = 0;
+    int nCellsVertical = 0;
+
+    /* **********Start constructors***************************/
+    public DrawView(Context context) {
+        super(context);
+        ini(context);
+    }
+
+    public DrawView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        ini(context);
+    }
+
+    public DrawView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        ini(context);
+    }
+
+    public void ini(Context context) {
+        Log.e("SurfaceViewFun", "ini");
+        surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        screenWidth = displayMetrics.widthPixels;
+        screenHeight = displayMetrics.heightPixels;
+    }
+
+    /* **********End constructors***************** */
+
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        drawThread = new DrawThread(getHolder());
+        drawThread.setRunning(true);
+        drawThread.start();
+        setFocusable(true);
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        boolean retry = true;
+        drawThread.setRunning(false);
+        while (retry) {
+            try {
+                drawThread.join();
+                retry = false;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    class DrawThread extends Thread implements GestureDetector.OnGestureListener {
+
+        private boolean running = false;
+        private SurfaceHolder surfaceHolder;
+        private GestureDetector mGestureDetector;
+
+        public DrawThread(SurfaceHolder surfaceHolder) {
+            this.surfaceHolder = surfaceHolder;
+
+            //brush:
+            paint = new Paint();
+            paint.setColor(Color.LTGRAY);
+            paint.setStrokeWidth(3);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setTextSize(64);
+            paint.setTextAlign(Paint.Align.CENTER);
+
+
+            rect = new Rect();
+
+
+        }
+
+
+        public void setRunning(boolean running) {
+            this.running = running;
+        }
+
+        @Override
+        public void run() {
+            Canvas canvas;
+
+            while (running) {
+                canvas = null;
+                try {
+                    canvas = surfaceHolder.lockCanvas(null);
+                    if (canvas == null)
+                        continue;
+
+
+
+                        /*  *************from here drawing: ************ */
+
+
+                    //background
+                    canvas.drawColor(Color.BLACK);
+
+
+                    //Rectangulars:
+                    nCellsHorizontal = ((screenWidth * 3) / CELL_WIDTH) + 2;
+                    nCellsVertical = ((screenHeight * 3) / CELL_HEIGHT) + 2;
+
+                    int startX = START_POSITION_X - screenWidth;
+                    int startY = START_POSITION_Y - screenHeight;
+                    for (int i = 0; i <= nCellsHorizontal; i++) {
+                        for (int j = 0; j <= nCellsVertical; j++) {
+                            rect.set(startX + (CELL_WIDTH * i)
+                                    , startY + (CELL_WIDTH * j)
+                                    , startX + (CELL_WIDTH * (i + 1))
+                                    , startY + (CELL_HEIGHT * (j + 1)));
+
+                            cell = new Cell(i - (screenWidth / CELL_WIDTH) - 1, -1 * (j - (screenHeight * 2 / CELL_HEIGHT)) - 2, rect);
+                            canvas.drawRect(rect, paint);
+                            canvas.drawText(cell.toString(), rect.centerX(), rect.centerY(), paint);
+
+                            //TODO setTextPositionInCell()
+                               /* // Подсчитаем размер текста
+                                mPaint.getTextBounds(text, 0, text.length(), mTextBoundRect);
+                                //mTextWidth = textBounds.width();
+                                // Используем measureText для измерения ширины
+                                mTextWidth = mPaint.measureText(text);
+                                mTextHeight = mTextBoundRect.height();
+
+                                canvas.drawText(text,
+                                        centerX - (mTextWidth / 2f),
+                                        centerY + (mTextHeight /2f),
+                                        mPaint
+                                );*/
+                        }
+                    }
+
+                    canvas.drawText(nCellsHorizontal + "x" + nCellsVertical, screenWidth - 250, screenHeight - 300, paint);
+                } finally {
+                    if (canvas != null) {
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+
+                    }
+                }
+            }
+
+        }
+
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return false;
+        }
+
+        @Override
+        public void onShowPress(MotionEvent e) {
+
+        }
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            return false;
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+
+            if (e1.getY() < e2.getY()) {
+                Log.d("Gesture ", " Scroll Down");
+            }
+            if (e1.getY() > e2.getY()) {
+                Log.d("Gesture ", " Scroll Up");
+            }
+            return true;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            if (e1.getX() < e2.getX()) {
+                Log.d("Gesture ", "Left to Right swipe: " + e1.getX() + " - " + e2.getX());
+                Log.d("Speed ", String.valueOf(velocityX) + " pixels/second");
+            }
+            if (e1.getX() > e2.getX()) {
+                Log.d("Gesture ", "Right to Left swipe: " + e1.getX() + " - " + e2.getX());
+                Log.d("Speed ", String.valueOf(velocityX) + " pixels/second");
+            }
+            if (e1.getY() < e2.getY()) {
+                Log.d("Gesture ", "Up to Down swipe: " + e1.getX() + " - " + e2.getX());
+                Log.d("Speed ", String.valueOf(velocityY) + " pixels/second");
+            }
+            if (e1.getY() > e2.getY()) {
+                Log.d("Gesture ", "Down to Up swipe: " + e1.getX() + " - " + e2.getX());
+                Log.d("Speed ", String.valueOf(velocityY) + " pixels/second");
+            }
+            return true;
+
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+}
+
---
+++ app/src/main/java/com/mdolzhansky/canvas_infinityscroll/MainActivity.java
@@ -0,0 +1,20 @@
+package com.mdolzhansky.canvas_infinityscroll;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // setContentView(new DrawView(this));
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
