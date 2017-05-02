352006949d1a90bf02ecc5b2c9e2f8d788f9da5c
mmdj <mdolzhansky@gmail.com>
2017-05-02 18:58:31 +0300

    2

--- app/src/main/java/com/mdolzhansky/canvas_infinityscroll/DrawView.java
+++ app/src/main/java/com/mdolzhansky/canvas_infinityscroll/DrawView.java
@@ -17,7 +17,7 @@ import android.view.SurfaceView;
 import com.mdolzhansky.canvas_infinityscroll.model.Cell;
 
 /**
- * Created by mmdj on 29.04.2017.
+ *  Created by mmdj on 29.04.2017.
  */
 
 class DrawView extends SurfaceView implements SurfaceHolder.Callback {
@@ -30,16 +30,15 @@ class DrawView extends SurfaceView implements SurfaceHolder.Callback {
     private Rect rect;
     private Cell cell;
 
-    private int CELL_WIDTH = 250;
+    private int CELL_WIDTH =250;
     private int CELL_HEIGHT = 250;
     private int START_POSITION_X = 0;
     private int START_POSITION_Y = 0;
     int nCellsHorizontal = 0;
     int nCellsVertical = 0;
 
-
-    private float mPosX = 0, mPosY = 0;  // will indicate how much we have panned. Use these to adjust the graphics
-    private float mScaleFactor = 1.f;  // indicate the scalling. Use this to adjust graphics
+    private float mPosX = 0, mPosY = 0;
+    private float mScaleFactor = 1.f;  // indicate the scalling
     private float mLastTouchX;
     private float mLastTouchY;
     private static final int INVALID_POINTER_ID = -1;
@@ -113,8 +112,15 @@ class DrawView extends SurfaceView implements SurfaceHolder.Callback {
     private class DrawThread extends Thread {
 
         private boolean running = false;
-        private SurfaceHolder surfaceHolder;
+        private final SurfaceHolder surfaceHolder;
         Canvas canvas;
+
+        boolean done;
+        long startTime;
+
+        float duration = 5 * 1000;
+        float arcSweep;
+
 
         public DrawThread(SurfaceHolder surfaceHolder) {
             this.surfaceHolder = surfaceHolder;
@@ -153,25 +159,54 @@ class DrawView extends SurfaceView implements SurfaceHolder.Callback {
                     //background
                     canvas.drawColor(Color.BLACK);
 
-
                     //Rectangulars:
-                    nCellsHorizontal = ((screenWidth * 3) / CELL_WIDTH) + 2;
-                    nCellsVertical = ((screenHeight * 3) / CELL_HEIGHT) + 2;
+                    setRect();
+                    startTime = System.currentTimeMillis();
+                    arcSweep = 0;
+                    done = false;
+                   // while (!done) {  // draw until done
+                       // updateArcSweep();
+                        //drawCurrentArc();
+                    //}
 
-                    int startX = START_POSITION_X - screenWidth;
-                    int startY = START_POSITION_Y - screenHeight;
-                    for (int i = 0; i <= nCellsHorizontal; i++) {
-                        for (int j = 0; j <= nCellsVertical; j++) {
-                            rect.set(startX + (CELL_WIDTH * i)
-                                    , startY + (CELL_WIDTH * j)
-                                    , startX + (CELL_WIDTH * (i + 1))
-                                    , startY + (CELL_HEIGHT * (j + 1)));
+                } finally {
+                    if (canvas != null) {
+                        surfaceHolder.unlockCanvasAndPost(canvas);
 
-                            cell = new Cell(i - (screenWidth / CELL_WIDTH) - 1, -1 * (j - (screenHeight * 2 / CELL_HEIGHT)) - 2, rect);
-                            canvas.drawRect(rect, paint);
-                            canvas.drawText(cell.toString(), rect.centerX(), rect.centerY(), paint);
+                    }
+                }
+            }
 
-                            //TODO setTextPositionInCell()
+        }
+
+        public void setRect() {
+
+
+
+            synchronized (surfaceHolder) {
+
+                float cellRealWidth = CELL_WIDTH * mScaleFactor;
+                float cellRealHeight = CELL_HEIGHT * mScaleFactor;
+
+                nCellsHorizontal = (int) (((screenWidth * 3) / cellRealWidth) + 2);
+                nCellsVertical = (int) (((screenHeight * 3) / cellRealHeight) + 2);
+
+                int startX = (int) (START_POSITION_X - screenWidth + mPosX);
+                int startY = (int) (START_POSITION_Y - screenHeight + mPosY);
+
+                for (int i = 0; i <= nCellsHorizontal; i++) {
+                    for (int j = 0; j <= nCellsVertical; j++) {
+
+                        rect.set((int) (startX + (cellRealWidth * i))                //left
+                                , (int) (startY + (cellRealHeight * j))              //top
+                                , (int) (startX + (cellRealWidth * (i + 1)))         //right
+                                , (int) (startY + (cellRealHeight * (j + 1))));      //bottom
+
+                      //  cell = new Cell(i - (screenWidth / CELL_WIDTH) - 1, -1 * (j - (screenHeight * 2 / CELL_HEIGHT)) - 2, rect);
+                        canvas.drawRect(rect, paint);
+
+                      //  canvas.drawText(cell.toString(), rect.centerX(), rect.centerY(), paint);
+                        //TODO setTextPositionInCell()
                                /* // Подсчитаем размер текста
                                 mPaint.getTextBounds(text, 0, text.length(), mTextBoundRect);
                                 //mTextWidth = textBounds.width();
@@ -184,19 +219,28 @@ class DrawView extends SurfaceView implements SurfaceHolder.Callback {
                                         centerY + (mTextHeight /2f),
                                         mPaint
                                 );*/
-                        }
-                    }
-
-                    canvas.drawText(nCellsHorizontal + "x" + nCellsVertical, screenWidth - 250, screenHeight - 300, paint);
-                } finally {
-                    if (canvas != null) {
-                        surfaceHolder.unlockCanvasAndPost(canvas);
-
                     }
                 }
-            }
 
+                canvas.drawText(nCellsHorizontal + "x" + nCellsVertical, screenWidth - 250, screenHeight - 300, paint);
+            }
         }
+
+      /*  void updateArcSweep() {
+            long currentTime = System.currentTimeMillis();
+            arcSweep = (float) ((currentTime - startTime) / duration * 360.0);
+            if (currentTime - startTime > duration) {
+                done = true;
+                arcSweep = 360;
+            }
+        }
+
+        void drawCurrentArc() {
+            canvas = surfaceHolder.lockCanvas(null); // must lock before drawing
+            canvas.drawColor(Color.BLACK);    // clear the screen
+        //    canvas.drawArc(rect, 0, arcSweep, true, drawingPaint);  // draw new stuff
+            surfaceHolder.unlockCanvasAndPost(canvas);  // must unlock when done
+        }*/
 
 
     }
@@ -237,42 +281,38 @@ class DrawView extends SurfaceView implements SurfaceHolder.Callback {
                     mLastTouchX = x;
                     mLastTouchY = y;
 
-// Invalidate to request a redraw
-// invalidate(); // use this is a regular canvas is being used
-                    drawThread.setOval();
-                    if (drawThread.done == true)
-                        drawThread.drawCurrentArc();
+                    drawThread.setRect();
+                  //  if (drawThread.done) drawThread.drawCurrentArc();
 
                 }
             }
+            break;
+            case MotionEvent.ACTION_UP: {
+                mActivePointerId = INVALID_POINTER_ID;
                 break;
-                case MotionEvent.ACTION_UP: {
-                    mActivePointerId = INVALID_POINTER_ID;
-                    break;
-                }
-                case MotionEvent.ACTION_CANCEL: {
-                    mActivePointerId = INVALID_POINTER_ID;
-                    break;
-                }
-                case MotionEvent.ACTION_POINTER_UP: {
+            }
+            case MotionEvent.ACTION_CANCEL: {
+                mActivePointerId = INVALID_POINTER_ID;
+                break;
+            }
+            case MotionEvent.ACTION_POINTER_UP: {
 // Extract the index of the pointer that left the touch sensor
-                    final int pointerIndex = (action & MotionEvent.ACTION_POINTER_INDEX_MASK)
-                            >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
-                    final int pointerId = ev.getPointerId(pointerIndex);
-                    if (pointerId == mActivePointerId) {
+                final int pointerIndex = (action & MotionEvent.ACTION_POINTER_INDEX_MASK)
+                        >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+                final int pointerId = ev.getPointerId(pointerIndex);
+                if (pointerId == mActivePointerId) {
 // This was our active pointer going up. Choose a new
 // active pointer and adjust accordingly.
-                        final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
-                        mLastTouchX = ev.getX(newPointerIndex);
-                        mLastTouchY = ev.getY(newPointerIndex);
-                        mActivePointerId = ev.getPointerId(newPointerIndex);
-                    }
-                    break;
+                    final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
+                    mLastTouchX = ev.getX(newPointerIndex);
+                    mLastTouchY = ev.getY(newPointerIndex);
+                    mActivePointerId = ev.getPointerId(newPointerIndex);
                 }
-            }// end switch statement
+                break;
+            }
+        }// end switch statement
         return true;// end onTouch
-        }
-
+    }
 
 
     public class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
@@ -285,9 +325,8 @@ class DrawView extends SurfaceView implements SurfaceHolder.Callback {
             mScaleFactor = Math.max(0.1f, Math.min(mScaleFactor, 5.0f));
 
 //invalidate(); // use this for a regular canvas (i.e., not a SurfaceView)
-            drawThread.setOval();
-            if (drawThread.done == true)
-                drawThread.drawCurrentArc();
+            drawThread.setRect();
+           // if (drawThread.done) drawThread.drawCurrentArc();
 
             return true;
         }
@@ -308,13 +347,17 @@ class DrawView extends SurfaceView implements SurfaceHolder.Callback {
 
         @Override
         public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            Log.e("SurfaceViewFun", "fling: started at (" + e1.getX() + " ," + e1.getY() + "). Ended at (" + e2.getX() + " ," + e2.getY() + "). With velocity (" + velocityX + " ," + velocityY + ")");
+            Log.e("SurfaceViewFun", "fling: started at (" + e1.getX() + " ," + e1.getY()
+                    + "). Ended at (" + e2.getX() + " ," + e2.getY() + "). With velocity ("
+                    + velocityX + " ," + velocityY + ")");
             return true;
         }
 
         @Override
-        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-            Log.e("SurfaceViewFun", "scroll: started at (" + e1.getX() + " ," + e1.getY() + "). Ended at (" + e2.getX() + " ," + e2.getY() + "). With total distance (" + distanceX + " ," + distanceY + ")");
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            Log.e("SurfaceViewFun", "scroll: started at (" + e1.getX() + " ," + e1.getY()
+                    + "). Ended at (" + e2.getX() + " ," + e2.getY()
+                    + "). With total distance (" + velocityX + " ," + velocityY + ")");
 
             if (e1.getX() < e2.getX()) {
                 Log.d("Gesture ", "Left to Right swipe: " + e1.getX() + " - " + e2.getX());
